f4c91f9dfff444a56b8950a7180dca33c67fa7c3
suman kumar <suman1ew09@gmail.com>
2024-11-29 10:05:20 +0530

    updated

--- src/test/java/stepdefinitions/AddRemoveElementsStepDef.java
+++ src/test/java/stepdefinitions/AddRemoveElementsStepDef.java
@@ -10,8 +10,8 @@ public class AddRemoveElementsStepDef {
     Add_remove_elements elementsPage = new Add_remove_elements(DriverFactory.getPage());
 
     @Given("user navigates to {string}")
-    public void user_navigates_to(String string) {
-
+    public void user_navigates_to(String string) throws Exception {
+      throw new Exception();
     }
     @When("user clicks on {string} icon in main page")
     public void user_clicks_on_icon_in_main_page(String string) {
