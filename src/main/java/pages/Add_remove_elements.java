1f9c98403c9e6a35fcbaf83f47240e450ff1e342
suman kumar <suman1ew09@gmail.com>
2024-11-29 11:41:13 +0530

    updated url

--- src/main/java/pages/Add_remove_elements.java
+++ src/main/java/pages/Add_remove_elements.java
@@ -5,6 +5,8 @@ import com.microsoft.playwright.Page;
 
 public class Add_remove_elements {
 
+
+
     private Page page;
     private final Locator FULL_NAME_EDITBOX;
 
@@ -12,6 +14,14 @@ public class Add_remove_elements {
     public Add_remove_elements(Page page) {
         this.page = page;
         this.FULL_NAME_EDITBOX = page.getByPlaceholder("Full Name");
+    }
+
+    public Page getPage() {
+        return page;
+    }
+
+    public void setPage(Page page) {
+        this.page = page;
     }
     public void clickElementNavBar(String navbarOption) {
         this.page.getByText(navbarOption, new Page.GetByTextOptions().setExact(true)).click();
--- src/test/java/stepdefinitions/AddRemoveElementsStepDef.java
+++ src/test/java/stepdefinitions/AddRemoveElementsStepDef.java
@@ -4,14 +4,16 @@ import factory.DriverFactory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import pages.Add_remove_elements;
+import utils.WebActions;
 
 public class AddRemoveElementsStepDef {
 
     Add_remove_elements elementsPage = new Add_remove_elements(DriverFactory.getPage());
 
     @Given("user navigates to {string}")
-    public void user_navigates_to(String string) throws Exception {
-      throw new Exception();
+    public void user_navigates_to(String url) throws Exception {
+        elementsPage.getPage().navigate(WebActions.getProperty(url));
+
     }
     @When("user clicks on {string} icon in main page")
     public void user_clicks_on_icon_in_main_page(String string) {
